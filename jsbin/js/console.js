49e2b51115ea6c1d40f028842921406d732f2568
Shahbaaz Hussain <shahbaazhussain@gmail.com>
2018-07-17 22:18:55 -0400

    javascript fundamentals

---
+++ jsbin/js/console.js
@@ -0,0 +1,33 @@
+// alert('Hello World!');
+
+// Single line comment
+
+/**
+ * Multi line
+ * comment
+ */
+
+// console.log('hello world!'); // print string
+// console.log(1234); // print number
+// console.log(true); // print boolean
+// var greeting = "Salaam";
+// console.log(greeting); // print a variable
+// console.log([1,2,3,4,5,6]); // print an array
+// console.log({a:1, b:2, c:3}); // print an object
+
+// console.table({a:1, b:2, c:3, d:4});
+
+console.error('This is an error message');
+console.warn('This is a warning');
+console.clear();
+console.time('check');
+console.log('Hi');
+var myObj = {
+  greeting: 'salaam',
+  repeat: 5
+};
+console.log(myObj);
+console.log(myObj);
+console.log(myObj);
+console.log(myObj);
+console.timeEnd('check');
---
+++ jsbin/js/variables.js
@@ -0,0 +1,67 @@
+// var, let and const
+// var name = "Sami Khan";
+// console.log(name);
+// name = "Irfan";
+// console.log(name);
+
+// // Initializing a variable
+// var greeting;
+// console.log(greeting);
+// greeting = "Salaam";
+// console.log(greeting);
+
+// naming variables
+// variables can include letters, numbers, _, $
+// variables cannot start with a number
+// var 123name = 'Harry'; // through an error
+// var name123 = 'Harry';
+// console.log(name123);
+// var _name = 'Henry';
+// console.log(_name);
+// var $name = 'Sajjad';
+// console.log($name);
+
+// Multi word variables
+var firstName = 'Brad'; // camel case (very good)
+var first_name = 'Paul'; // Underscore
+var FirstName = 'Aaquib'; // Pascal case
+var firstname; // small case
+
+// let is block scoped
+// let name;
+// console.log(name);
+// name = 'Peter pan';
+// console.log(name);
+// name = 'Hannah';
+// console.log(name);
+
+// const is also block scoped
+// {
+//   let greeting = 'hi';
+//   console.log(greeting);
+// }
+// console.log(greeting);
+
+const name = 'John';
+console.log(name);
+// name = 'Johny';
+// const greeting; // const must be assigned some value
+const person = {
+  name: 'John',
+  age: 30
+};
+console.log(person);
+// person.name = 'Hannah';
+// console.log(person);
+// Below line is not allowed
+// person = {
+//   name: 'Hannah',
+//   age: 34
+// };
+const numbers = [1,2,3,4,5];
+console.log(numbers);
+//numbers = [1,2,3,4,5,6];
+numbers.push(6);
+numbers.unshift(0);
+console.log(numbers);
+
