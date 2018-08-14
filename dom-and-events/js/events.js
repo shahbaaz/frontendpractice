f4191c76446ec2d50ee22d434056cafd4db257bd
Shahbaaz Hussain <shahbaazhussain@gmail.com>
2018-08-13 22:10:33 -0400

    javacript events

---
+++ dom-and-events/js/events.js
@@ -0,0 +1,39 @@
+// document.querySelector('.clear-tasks').addEventListener('click', function(event) {
+//   console.log('Button is clicked!');
+//   event.preventDefault();
+// });
+
+document.querySelector('.clear-tasks').addEventListener('click', handleClick);
+
+function handleClick(event) {
+  // console.log('Click event happened!');
+  event.preventDefault();
+  let val;
+  val = event;
+
+  // Event target element
+  val = event.target;
+  val = event.target.id;
+  val = event.target.className;
+  val = event.target.classList;
+
+  event.target.innerText = 'CLEAR TODO ITEMS';
+
+  // Event type
+  val = event.type;
+
+  // Timestamp
+  val = event.timeStamp;
+  console.log(event);
+
+  // Coordinates 
+  val = event.clientY;
+  val = event.clientX;
+
+  // Offset
+  val = event.offsetY;
+  val = event.offsetX;
+  
+
+  console.log(val);
+}
